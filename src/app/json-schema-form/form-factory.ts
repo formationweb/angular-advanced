7813ef45d81c5957ce9d0ba3359d44c8550945fe
RSamaium <sam@newbot.io>
2026-09-09 16:54:51 +0200

    feat: refactor login component to use FormFactory for form creation and update form controls

---
+++ src/app/json-schema-form/form-factory.ts
@@ -0,0 +1,45 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { inject, Injectable } from "@angular/core";
+
+export type Schema = {
+    type: 'string' | 'number' | 'object' | 'boolean'
+    properties?: Record<string, Schema>,
+    required?: string[]
+    minLength?: number
+    minimum?: number
+    format?: string
+}
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FormFactory {
+    private readonly formBuilder = inject(FormBuilder)
+
+    createForm(schema: Schema): FormGroup {
+        const createGroup = (groupObj: Schema) => {
+            let group: Record<string, any> = {}
+            for (let fieldName in groupObj.properties) {
+                const schemaProp = groupObj.properties[fieldName]
+                if (schemaProp.type == 'object') {
+                    group[fieldName] = createGroup(schemaProp)
+                }
+                else {
+                    const validators = []
+
+                    if (schemaProp.minLength) {
+                        validators.push(Validators.minLength(schemaProp.minLength))
+                    }
+                    if (schema.required?.includes(fieldName)) {
+                        validators.push(Validators.required)
+                    }
+                    
+                    group[fieldName] = ['', validators]
+                }
+            }
+            return this.formBuilder.group(group)
+        }
+        return createGroup(schema)
+    }
+}
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,7 +1,31 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from './auth';
 import { Router } from '@angular/router';
+import { FormFactory } from '../json-schema-form/form-factory';
+
+const loginSchema = {
+  type: 'object',
+  properties: {
+    password: { type: 'string', minLength: 2 },
+    email: { type: 'string', format: 'email' },
+    address: {
+      type: 'object',
+      properties: {
+        city: { type: 'string' },
+        zipcode: { type: 'string', minLength: 5 },
+      },
+      required: ['city'],
+    },
+  },
+  required: ['password', 'email'],
+};
 
 @Component({
   imports: [ReactiveFormsModule],
@@ -10,19 +34,23 @@ import { Router } from '@angular/router';
   templateUrl: './login.html',
 })
 export class Login {
-  private authService = inject(AuthService)
-  private router = inject(Router)
-  emailControl = new FormControl()
-  passControl = new FormControl()
-  form = new FormGroup({
-    email: this.emailControl,
-    password: this.passControl
-  })
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private builder = inject(FormBuilder);
+  private formFactory = inject(FormFactory);
+  form = this.formFactory.createForm(loginSchema as any);
+  // emailControl = new FormControl()
+  // passControl = new FormControl()
+  // form = new FormGroup({
+  //   email: this.emailControl,
+  //   password: this.passControl
+  // })
 
   login() {
-    const { email, password } = this.form.value
-    this.authService.login(email, password).subscribe(() => {
-      this.router.navigateByUrl('/')
-    })
+    const { email, password } = this.form.value as { email: string; password: string };
+    console.log(email, password);
+    // this.authService.login(email, password).subscribe(() => {
+    //   this.router.navigateByUrl('/');
+    // });
   }
 }
