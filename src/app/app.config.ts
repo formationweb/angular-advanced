790a6f03d0074a29680cc9a2b6a657c9a56960f2
RSamaium <sam@newbot.io>
2026-09-09 14:16:32 +0200

    feat: enhance Users component with search functionality and update app configuration for router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth';
@@ -7,7 +7,7 @@ import { authInterceptor } from './core/interceptors/auth';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,26 +1,61 @@
-import { ChangeDetectionStrategy, Component, computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, DestroyRef, effect, inject, input, linkedSignal, OnInit, resource, signal } from '@angular/core';
 import { UserService } from './user';
 import { User } from '../core/interfaces/user';
-import { rxResource, takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { rxResource, takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { UserCard } from "./user-card/user-card";
 import { interval } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { httpResource } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  imports: [UserCard, AsyncPipe],
+  imports: [UserCard, AsyncPipe, FormsModule],
   selector: 'app-users',
   templateUrl: './users.html'
 })
 export class Users {
   private userService = inject(UserService)
   private destroyRef = inject(DestroyRef)
-  users$ =  this.userService.getAll().pipe(takeUntilDestroyed(this.destroyRef))
-  // protected readonly users = toSignal(this.userService.getAll())
+  private route = inject(ActivatedRoute)
+  private router = inject(Router)
+
+  search = input('')
+
+  //users$ =  this.userService.getAll().pipe(takeUntilDestroyed(this.destroyRef))
+  protected readonly users = toSignal(this.userService.getAll())
   //  protected readonly usersResource = rxResource({
   //   stream: () => {
   //     return this.userService.getAll()
   //   }
   // })
-  // protected readonly loading = computed(() => this.usersResource.isLoading())
+  protected readonly searchValue = linkedSignal(() => this.search())
+  // protected readonly usersResource = resource({
+  //   params: () => {
+  //     return {
+  //       search: this.searchValue()
+  //     }
+  //   },
+  //   loader: ({ params, abortSignal }) => {
+  //     return fetch('https://jsonplaceholder.typicode.com/users?search=' + params.search, {
+  //       signal: abortSignal
+  //     }).then(res => res.json()) 
+  //   }
+  // })
+  protected readonly usersResource = httpResource<User[]>(
+      () => 'https://jsonplaceholder.typicode.com/users?search=' + this.search())
+  protected readonly loading = computed(() => this.usersResource.isLoading())
 
+    constructor() {
+      // this.route.queryParamMap.subscribe(map => {
+      //   const search = map.get('search')
+      //   console.log(search)
+      // })
+      effect(() => {
+         this.router.navigate([], {
+           queryParams: { search: this.searchValue() },
+           replaceUrl: true
+         })
+      })
+    }
 }
