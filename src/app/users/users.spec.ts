f8d6e6a71a4a12658616d63641aad6e6be638138
RSamaium <sam@newbot.io>
2026-09-10 15:12:27 +0200

    feat: update Users component to include test attribute for user cards and enhance unit tests for user count

--- src/app/users/users.spec.ts
+++ src/app/users/users.spec.ts
@@ -3,6 +3,9 @@ import { Users } from './users';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { User } from '../core/interfaces/user';
 import { UserService } from './user';
+import { UserStore } from '../store/user.store';
+import { patchState } from '@ngrx/signals';
+import { unprotected } from '@ngrx/signals/testing';
 
 // class UserServiceMock {
 //     getAll(): Observable<User[]> {
@@ -20,6 +23,7 @@ describe('Tester MyTestComponent', () => {
   let fixture: ComponentFixture<Users>;
   let comp: Users;
   let el: HTMLElement;
+  let store: InstanceType<typeof UserStore>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -45,7 +49,27 @@ describe('Tester MyTestComponent', () => {
     comp = fixture.componentInstance;
     await fixture.whenStable();
     el = fixture.nativeElement;
+    store = TestBed.inject(UserStore)
   });
+
+  it('Calculer nbUsers lorsque users changent', () => {
+     expect(store.nbUsers()).toBe(1)
+    patchState(unprotected(store), {
+        users: [
+          {
+              id: 1,
+              name: 'ana',
+              email: 'ana@gmail.com'
+          },
+          {
+              id: 2,
+              name: 'ben',
+              email: 'ben@gmail.com'
+          }
+      ]
+    })
+    expect(store.nbUsers()).toBe(2)
+  })
 
   it('tester liste utilisateur', async () => {
     const userEl = el.querySelectorAll('[test-users]');
