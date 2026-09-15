0b708c533ff659a9a5f93ee058d24916d91ebef9
RSamaium <sam@newbot.io>
2026-09-15 12:21:37 +0200

    feat: add Video component with video playback functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Video } from './video/video';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Video],
   selector: 'app-root',
   styleUrl: './app.css',
-  template: `<router-outlet />`
+  template: `<app-video />`
 })
 export class App {}
