9de05e2f23006d428050e36eb08d99a1c4578193
RSamaium <sam@newbot.io>
2026-09-15 16:15:49 +0200

    refactor: update login component to use FormBuilder for form management and improve validation

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,12 +1,46 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Auth } from './auth';
 import { Router } from '@angular/router';
 
+export const userSchema = {
+  type: 'object',
+  properties: {
+    name: { type: 'string', minLength: 2 },
+    email: { type: 'string', format: 'email' },
+
+    address: {
+      type: 'object',
+      properties: {
+        city: { type: 'string' },
+        zipcode: { type: 'string', minLength: 5 },
+      },
+      required: ['city'],
+    },
+  },
+
+  required: ['name', 'email'],
+};
+
+export const loginSchema = {
+  type: 'object',
+  properties: {
+    password: { type: 'string', minLength: 2 },
+    email: { type: 'string', format: 'email' },
+  },
+  required: ['password', 'email'],
+};
+
 type LoginPayload = {
-  email: string
-  password: string
-}
+  email: string;
+  password: string;
+};
 
 @Component({
   imports: [ReactiveFormsModule],
@@ -15,19 +49,21 @@ type LoginPayload = {
   templateUrl: './login.html',
 })
 export class Login {
-  private auth = inject(Auth)
-  private router = inject(Router)
-  emailControl = new FormControl('')
-  passControl = new FormControl('')
-  form = new FormGroup({
-    email: this.emailControl,
-    password: this.passControl
-  })
+  private auth = inject(Auth);
+  private router = inject(Router);
+  private builder = inject(FormBuilder);
+  form = this.builder.group({
+    email: ['', [Validators.required, Validators.minLength(2)]],
+    password: '',
+    address: this.builder.group({
+      city: '',
+    }),
+  });
 
   login() {
-      const { email, password } = this.form.value as LoginPayload
-      this.auth.login(email, password).subscribe(() => {
-         this.router.navigateByUrl('/')
-      })
+    const { email, password } = this.form.value as LoginPayload;
+    this.auth.login(email, password).subscribe(() => {
+      this.router.navigateByUrl('/');
+    });
   }
 }
